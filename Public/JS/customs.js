ccf6ac1a336a3ee4b65538004218bb7fe9f4f63b
Redemter <redemterrita@gmail.com>
2023-07-20 17:52:42 +0300

    updating image text on what we do section

--- Public/JS/customs.js
+++ Public/JS/customs.js
@@ -1,15 +1,12 @@
-jQuery(selector). action();
-
-//selector id # class . tag name this
- 
-$(document).ready(()=>{ //ensure html page loads before loading jquery code.
-    $('#one').click(event => {
-        alert ("you have clicked paragraph 1");
-        console.log(event);
-    });
-});
-
-$(()=>{ //simplified ready function
-    //jquery code
-
-})
+function showOne () { // click on the first image
+    var div = document.getElementById("div1"); // get the div
+    div.innerHTML = " Our design practice offers a full range of services including brand strategy, interaction and visual design and user experience testing. Throughout your project, our designers create and implement visual design and workflows, solicit user feedback and work with you to make sure what gets built is what is needed."; // change the text (inner html)
+  }
+  function showTwo () {
+    var div = document.getElementById("div2"); // get the div
+    div.innerHTML = " All engineers are fluent in the latest enterprise, mobile and web development technologies. They collaborate with your team to write, and improve code on a daily basis, using proven practices such as test-driven development and pair programming."
+  }
+  function showThree () {
+    var div = document.getElementById("div3"); // get the div
+    div.innerHTML = " Planning and development is iterative. Because we are constantly coding and testing, the products we build are always ready to go live. This iterative process allows for changes as business requirements evolve."
+  }
